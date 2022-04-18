ea769ccaa68e09cc474a0484c303413002b6baa4
giuse <giuseppecoluzzi87@gmail.com>
2022-04-18 10:01:43 +0200

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Budgeting</h1>;
+    return (
+        <>
+            <Navbar />
+        </>
+    );
 }
