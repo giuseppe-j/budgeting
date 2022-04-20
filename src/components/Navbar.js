3f6745478fdad1acf5b145b884c60a1d6452160d
giuse <giuseppecoluzzi87@gmail.com>
2022-04-20 16:20:20 +0200

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,48 @@
 import { useState } from "react";
 import { SearchIcon, MenuIcon } from "@heroicons/react/solid";
+import logo from "../images/monefy_logo.png";
 
 export default function Navbar() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-  return (
-    <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white"
-              href="#home"
-            >
-              <h6 className="text-xl font-normal leading-normal mt-0 mb-2 text-white-800">
-                Budgeting
-              </h6>
-            </a>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <MenuIcon className="h-5 w-5 text-white-500" />
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#search"
-                >
-                  <SearchIcon className="h-5 w-5 text-white-500" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <>
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-500 mb-3">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <a
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white"
+                            href="#home"
+                        >
+                            <img className="w-20" src={logo} alt="Monefy" />
+                        </a>
+                        <button
+                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <MenuIcon className="h-5 w-5 text-white-500" />
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id="example-navbar-danger"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="nav-item">
+                                <a
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    href="#search"
+                                >
+                                    <SearchIcon className="h-5 w-5 text-white-500" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
