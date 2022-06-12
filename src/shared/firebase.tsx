592dba826327940599529ad3c4702879d089f1e8
giuse <giuseppecoluzzi87@gmail.com>
2022-06-12 11:35:44 +0200

    add dotenv

--- src/shared/firebase.tsx
+++ src/shared/firebase.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "budgeting-app-6f390.firebaseapp.com",
+    projectId: "budgeting-app-6f390",
+    storageBucket: "budgeting-app-6f390.appspot.com",
+    messagingSenderId: "772618059676",
+    appId: "1:772618059676:web:02f76a36a879bc6fd21259",
 };
 
 const app = initializeApp(firebaseConfig);
